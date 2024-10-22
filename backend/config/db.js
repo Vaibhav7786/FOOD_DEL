5b70d2e4106fae115663996bf0687cfd4bae74eb
Vaibhav7786 <vaibhavsonar7786@gmail.com>
2024-10-22 16:11:05 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async() =>{
+    await mongoose.connect('mongodb+srv://webstack:77557755@cluster0.uh6fz.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+}
